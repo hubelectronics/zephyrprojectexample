a539c37c7be8f6d8c016dcb82fcd59ae8e641465
rubenmendezingenieria <ruben.ingenieria@hub-electronics.com>
2024-11-14 16:33:50 +0100

    add vendor prefixes, access to dts not working good

--- application/kosmos200.cpp
+++ application/kosmos200.cpp
@@ -1,9 +1,14 @@
 #include "kosmos200.hpp"
 
 Kosmos200::Kosmos200(){
-    led_remote_rx = new DT_LED_SIMPLE_INSTANCE();
+    led_remote_rx = new DT_LED_SIMPLE_INSTANCE(led_remote_rx);
+
+
+    //const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_PATH(led_remote_rx), enable_gpios);
+
+    //led_remote_rx = new LedSimple(&led);
 }
 
 Kosmos200::~Kosmos200() {
-    delete led_remote_rx;
+   delete led_remote_rx;
 }
